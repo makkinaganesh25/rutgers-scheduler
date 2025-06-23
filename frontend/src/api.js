c632f676b9dc2b46e803c15b6eb8563bcf2d6d30
Ganesh Makkina <154107623+makkinaganesh25@users.noreply.github.com>
2025-06-23 15:49:28 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -193,7 +193,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';
+const API_BASE = process.env.REACT_APP_API_BASE_URL;
 const api = axios.create({
   baseURL: API_BASE,
   headers: { 'Content-Type': 'application/json' },
